ebc91fbd963c0a80fadb8b49e01232dbe41fb2d6
Adolfo Despina <adespina@at-h4.21-school.ru>
2020-02-20 19:47:24 +0300

    changes

---
+++ Matcha/header.php
@@ -0,0 +1,9 @@
+<header>
+	<div class="top_header">
+  	<ul class="menu">
+    	<li class = "item"><a href='".$root."../index.php'><img src="../img/home.png" alt=""/></a></li>
+    	<li class = "item"><a href='".$root."'><img src="../img/chat.png" alt=""/></a></li>
+    	<li class = "item"><a href='".$root."'><img src="../img/match.png" alt=""/></a></li>
+		<li class = "item_logo" style='float:right'><a href='".$root."'><img class = "logo" src="../img/logo.png" alt=""/></span></a></li>
+	</ul>
+</header>
